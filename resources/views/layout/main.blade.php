0e74b64989eba84d993e16df4c5df2b0abc5eeeb
Estermanalu <138660627+Estermanalu@users.noreply.github.com>
2023-07-06 14:49:55 +0700

    edit

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,10 +8,7 @@
     <link href="{{ asset('/') }}assets/plugins/fontawesome/css/all.min.css" rel="stylesheet" >
   </head>
   <body>
-    <p>cfsfhjfrcjgft</p>
-
-    <h5>kkkkkkkk</h5>
-
+   <h5>Welcome To SiAP (Sistem Absensi Pegawai)</h5>
     <script src="{{ asset('/') }}assets/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     
 </body>
