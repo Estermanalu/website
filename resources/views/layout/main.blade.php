2407c7eb2c20564b3beaa5ef44df9b7fc8de51c4
Estermanalu <estermanalu31@gmail.com>
2023-07-06 15:03:02 +0700

    tes

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,6 +10,8 @@
   <body>
     <h1>sjbfsjebfhmsdjbc</h1>
 
+    <h3>lllllll</h3>
+
     <script src="{{ asset('/') }}assets/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     
 </body>
