80972b65efb0cb1893471a03b087b8257365576f
Estermanalu <estermanalu31@gmail.com>
2023-07-06 15:13:17 +0700

    home

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+      WELCOME
+    </div>
+    <div class="card-body">
+     <div class="alert alert-info">
+        Selamat Datang di Website Laravel Ester Manalu
+     </div>
+    </div>
+  </div>
+@endsection
