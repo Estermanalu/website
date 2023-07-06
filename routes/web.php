6a953158664347b331255516ee124c8fed6cd693
Estermanalu <estermanalu31@gmail.com>
2023-07-06 15:19:26 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.main');
 });
+
+Route::get('/home', function () {
+    return view('layout.home');
+});
