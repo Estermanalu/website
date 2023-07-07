d39c7a2aff0c7e102c52db76d52ea38e7b753d16
Estermanalu <estermanalu31@gmail.com>
2023-07-07 16:10:57 +0700

    update

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+      WELCOME
+    </div>
+    <div class="card-body">
+     <div class="alert alert-info">
+        Selamat Datang di Website tty
+     </div>
+    </div>
+  </div>
+@endsection
