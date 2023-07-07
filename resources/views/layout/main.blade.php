84ec9e3851f705836d7c40a9fdf84b38d3334654
Estermanalu <estermanalu31@gmail.com>
2023-07-07 15:46:04 +0700

    update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,19 +8,7 @@
   <link href="{{ asset('/') }}assets/plugins/fontawesome/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    <!-- Bagian header layout -->
-    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
-   
-    <!-- Bagian sidebar layout -->
-    
-
-    <!-- Bagian konten layout -->
-    <div class="content">
-        @yield('content')
-    </div>
-
-    <!-- Bagian footer layout -->
-
+  
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg bg-info navbar-dark">
     <div class="container">
