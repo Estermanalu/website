ffc43b17538a7b4c520f62b1953184714845016b
Rezki_Amalia <rezki.amallia56@gmail.com>
2023-07-07 15:07:34 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link href="{{ asset('/') }}assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{ asset('/') }}assets/plugins/fontawesome/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
@@ -39,6 +40,6 @@
             </div>
         </div>
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('/') }}assets/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
