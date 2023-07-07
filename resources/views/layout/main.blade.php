7ac66a6e40fa129f2a9dce0acfe0f7232963afbe
Estermanalu <estermanalu31@gmail.com>
2023-07-07 15:44:55 +0700

    update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -12,12 +12,7 @@
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
    
     <!-- Bagian sidebar layout -->
-    <div class="sidebar">
-      <a href="#" class="active">Menu 1</a>
-      <a href="#">Menu 2</a>
-      <a href="#">Menu 3</a>
-      <a href="#">Menu 4</a>
-  </div>
+    
 
     <!-- Bagian konten layout -->
     <div class="content">
